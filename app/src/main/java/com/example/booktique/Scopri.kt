8489aebe0bf3f23ee52b5249cd3159a948334ee1
Laura Ferretti <s1098036@studenti.univpm.it>
2023-06-27 13:00:28 +0200

    modifiche

--- app/src/main/java/com/example/booktique/Scopri.kt
+++ app/src/main/java/com/example/booktique/Scopri.kt
@@ -1,5 +1,6 @@
 package com.example.booktique
 
+import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
@@ -196,7 +198,18 @@ class Scopri : Fragment() {
 
                     .into(targetButtons[i])
 
+                setupImageButtonClickListener(book, targetButtons[i])
+
             }
+        }
+    }
+
+
+    private fun setupImageButtonClickListener(book: VolumeDet, imageButton: ImageButton) {
+        imageButton.setOnClickListener {
+            val intent = Intent(requireContext(), DettaglioLibro::class.java)
+            intent.putExtra("book", book) // Passa l'ID del libro all'Activity
+            startActivity(intent)
         }
     }
 
