84491e135d19b4c6bfc52a417562a5af1c7566fe
Laura Ferretti <s1098036@studenti.univpm.it>
2023-07-05 00:27:43 +0200

    cc

--- app/src/main/java/com/example/booktique/CatalogoHome.kt
+++ app/src/main/java/com/example/booktique/CatalogoHome.kt
@@ -1,6 +1,5 @@
 package com.example.booktique
 
-import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
@@ -10,20 +9,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.Toast
-import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 import com.example.booktique.databinding.FragmentCatalogoHomeBinding
-import com.example.booktique.AutenticazioneActivity
-import com.example.booktique.FragmentUtils.replaceFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -256,7 +251,7 @@ class CatalogoHome : Fragment() {
 
                         .into(imageButtons[i])
 
-                    //setupImageButtonClickListener(book, targetButtons[i])
+                    setupImageButtonClickListenerDaLeggere(book, imageButtons[i])
 
                 }
             }
@@ -375,8 +370,7 @@ class CatalogoHome : Fragment() {
 
                         .into(imageButtons[i])
 
-                    //setupImageButtonClickListener(book, targetButtons[i])
-
+                    setupImageButtonClickListenerLetto(book, imageButtons[i])
                 }
             }
         }
@@ -386,9 +380,26 @@ class CatalogoHome : Fragment() {
 
     private fun setupImageButtonClickListener(book: LibriInC, imageButton: ImageButton) {
         imageButton.setOnClickListener {
-            BookHolder.libroInc = book
+            val navController = findNavController()
+            val action = CatalogoHomeDirections.actionCatalogoHomeToLibroInCorso(book)
+            findNavController().navigate(action)
+        }
+    }
+
+    private fun setupImageButtonClickListenerDaLeggere(book: LibriDaL, imageButton: ImageButton) {
+        imageButton.setOnClickListener {
+
+            val navController = findNavController()
+            val action = CatalogoHomeDirections.actionCatalogoHomeToLibroDaLeggere(book)
+            findNavController().navigate(action)
+        }
+    }
+
+    private fun setupImageButtonClickListenerLetto(book: LibriL, imageButton: ImageButton) {
+        imageButton.setOnClickListener {
+            BookHolder.libroL = book
             val fragmentManager = requireActivity().supportFragmentManager
-            val scopriFragment = DettaglioLibroInCorso.newInstance(book)
+            val scopriFragment = LibroLetto.newInstance(book)
             fragmentManager.beginTransaction()
                 .replace(R.id.fragmentContainerView, scopriFragment)
                 .commit()
