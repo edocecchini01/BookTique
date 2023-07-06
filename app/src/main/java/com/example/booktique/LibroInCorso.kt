6dc9a0b0e35e980740edff581469f788ca8029d4
edocecchini01 <S1101827@studenti.univpm.it>
2023-07-06 18:28:48 +0200

    dettagli libri

--- app/src/main/java/com/example/booktique/LibroInCorso.kt
+++ app/src/main/java/com/example/booktique/LibroInCorso.kt
@@ -1,24 +1,36 @@
 package com.example.booktique
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.SeekBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.booktique.databinding.FragmentLibroInCorsoBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class LibroInCorso : Fragment() {
     private lateinit var binding: FragmentLibroInCorsoBinding
     private lateinit var cUser : FirebaseUser
     private lateinit var libroIncorso: LibriInC
+    private lateinit var activity : FragmentActivity
 
     private val args by navArgs<LibroInCorsoArgs>()
 
@@ -45,16 +57,117 @@ class LibroInCorso : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        activity = requireActivity()
 
-            val imageView = binding.imageView3
-            Glide.with(this)
-                .load(args.LibroInC.copertina)
-                .into(imageView)
+        val imageView = binding.imageView3
+        Glide.with(this)
+            .load(args.LibroInC.copertina)
+            .into(imageView)
 
-            binding.textView22.text = args.LibroInC.titolo
-            binding.textView23.text = args.LibroInC.autori
-            binding.textView13.text = args.LibroInC.descrizione
+        binding.textView22.text = args.LibroInC.titolo
+        binding.textView23.text = args.LibroInC.autori
+        binding.textView13.text = args.LibroInC.descrizione
 
+        binding.textView26.text = args.LibroInC.pagineTot.toString()
+        binding.seekBar4.max = args.LibroInC.pagineTot!!
+        binding.seekBar4.progress = args.LibroInC.paginaAtt!!
+        binding.textView25.text = args.LibroInC.paginaAtt.toString()
+
+        val bookId = args.LibroInC.id
+
+        binding.seekBar4.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seekBar: SeekBar?,
+                progress: Int,
+                fromUser: Boolean
+            ) {
+                binding.textView25.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                if (FirebaseAuth.getInstance().currentUser != null) {
+                    val cUser = FirebaseAuth.getInstance().currentUser!!
+                    Log.d("TAG", "Sono :")
+                    val database =
+                        FirebaseDatabase.getInstance("https://booktique-87881-default-rtdb.europe-west1.firebasedatabase.app/")
+                    val usersRef = database.reference.child("Utenti")
+                    val childRef = usersRef.child(cUser.uid)
+                    val catalogoRef = childRef.child("Catalogo")
+                    val inCorsoRef = catalogoRef.child("InCorso")
+
+                    if (bookId != null) {
+                        val pagAconv = binding.textView25.text.toString()
+                        inCorsoRef.child(bookId).child("paginaAtt").setValue(pagAconv.toInt())
+                    }
+                }
+
+                if(binding.textView25.text == binding.textView26.text && bookId != null){
+                    moveBooks(bookId)
+                    Toast.makeText(
+                        activity,
+                        "Complimenti hai terminato la tua lettura!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+        })
 
     }
+
+    private fun moveBooks(bookId : String) {
+        if (FirebaseAuth.getInstance().currentUser != null) {
+            val cUser = FirebaseAuth.getInstance().currentUser!!
+            val database =
+                FirebaseDatabase.getInstance("https://booktique-87881-default-rtdb.europe-west1.firebasedatabase.app/")
+            val usersRef = database.reference.child("Utenti")
+            val childRef = usersRef.child(cUser.uid)
+            val catalogoRef = childRef.child("Catalogo")
+            val daLeggereRef = catalogoRef.child("DaLeggere")
+            val inCorsoRef = catalogoRef.child("InCorso")
+            val lettiRef = catalogoRef.child("Letti")
+
+            Log.d("TAG", "bookId: $bookId")
+
+                inCorsoRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        for (childSnapshot in dataSnapshot.children) {
+                            val libro = childSnapshot.getValue(LibriDaL::class.java)
+
+                            if (libro != null && libro.id == bookId) {
+
+                                // Hai individuato il libro desiderato
+                                Log.d("Libro", "Libro trovato: $libro")
+                                lettiRef.child(bookId).setValue(libro)
+                                val libroRef = childSnapshot.ref
+                                Log.d("Libro", "Libro da eliminare: $libro")
+                                libroRef.removeValue()
+
+                                Toast.makeText(
+                                    activity,
+                                    "${libro.titolo?.take(50)}, spostato in \"Letti\"",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+
+                                val navController = Navigation.findNavController(activity, R.id.fragmentContainerView)
+                                navController.navigate(R.id.action_libroInCorso_to_catalogoLetti)
+
+                                break
+                            }
+                        }
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        TODO("Not yet implemented")
+                    }
+
+                })
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/booktique/LibroLetto.kt
+++ app/src/main/java/com/example/booktique/LibroLetto.kt
@@ -1,22 +1,31 @@
 package com.example.booktique
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.booktique.databinding.FragmentLibroInCorsoBinding
 import com.example.booktique.databinding.FragmentLibroLettoBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
 
 class LibroLetto : Fragment() {
     private lateinit var binding: FragmentLibroLettoBinding
     private lateinit var cUser : FirebaseUser
     private lateinit var libroLet: LibriL
+    private lateinit var activity : FragmentActivity
 
     private val args by navArgs<LibroLettoArgs>()
 
@@ -42,15 +51,163 @@ class LibroLetto : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        activity = requireActivity()
+        var valutazione = args.LibroLett.valutazione
+        var review = args.LibroLett.recensione
 
-            val imageView = binding.imageView3
-            Glide.with(this)
-                .load(args.LibroLett.copertina)
-                .into(imageView)
+        val imageView = binding.imageView3
+        Glide.with(this)
+            .load(args.LibroLett.copertina)
+            .into(imageView)
 
-            binding.textView22.text = args.LibroLett.titolo
-            binding.textView23.text = args.LibroLett.autori
-            binding.textView13.text = args.LibroLett.descrizione
+        binding.textView22.text = args.LibroLett.titolo
+        binding.textView23.text = args.LibroLett.autori
+        binding.textView13.text = args.LibroLett.descrizione
 
+        binding.textInputLayout.editText?.setText(review)
+
+        graphicLike(valutazione)
+
+        val bookId = args.LibroLett.id
+
+
+        binding.likeL.setOnClickListener {
+            if (valutazione == 1) {
+                if (FirebaseAuth.getInstance().currentUser != null) {
+                    val cUser = FirebaseAuth.getInstance().currentUser!!
+                    Log.d("TAG", "Sono :")
+                    val database =
+                        FirebaseDatabase.getInstance("https://booktique-87881-default-rtdb.europe-west1.firebasedatabase.app/")
+                    val usersRef = database.reference.child("Utenti")
+                    val childRef = usersRef.child(cUser.uid)
+                    val catalogoRef = childRef.child("Catalogo")
+                    val lettiRef = catalogoRef.child("Letti")
+
+                    if (bookId != null) {
+                            valutazione = 0
+                        graphicLike(valutazione)
+                        lettiRef.child(bookId).child("valutazione").setValue(valutazione)
+                    }
+                }
+            }else{
+                if (FirebaseAuth.getInstance().currentUser != null) {
+                    val cUser = FirebaseAuth.getInstance().currentUser!!
+                    Log.d("TAG", "Sono :")
+                    val database =
+                        FirebaseDatabase.getInstance("https://booktique-87881-default-rtdb.europe-west1.firebasedatabase.app/")
+                    val usersRef = database.reference.child("Utenti")
+                    val childRef = usersRef.child(cUser.uid)
+                    val catalogoRef = childRef.child("Catalogo")
+                    val lettiRef = catalogoRef.child("Letti")
+
+                    if (bookId != null) {
+                            valutazione = 1
+                        graphicLike(valutazione)
+                        lettiRef.child(bookId).child("valutazione").setValue(valutazione)
+                    }
+                }
+            }
+        }
+
+        binding.dislikeL.setOnClickListener {
+            if (valutazione == 2) {
+                if (FirebaseAuth.getInstance().currentUser != null) {
+                    val cUser = FirebaseAuth.getInstance().currentUser!!
+                    Log.d("TAG", "Sono :")
+                    val database =
+                        FirebaseDatabase.getInstance("https://booktique-87881-default-rtdb.europe-west1.firebasedatabase.app/")
+                    val usersRef = database.reference.child("Utenti")
+                    val childRef = usersRef.child(cUser.uid)
+                    val catalogoRef = childRef.child("Catalogo")
+                    val lettiRef = catalogoRef.child("Letti")
+
+                    if (bookId != null) {
+                            valutazione = 0
+                        graphicLike(valutazione)
+                        lettiRef.child(bookId).child("valutazione").setValue(valutazione)
+                    }
+
+                }
+            }else{
+                if (FirebaseAuth.getInstance().currentUser != null) {
+                    val cUser = FirebaseAuth.getInstance().currentUser!!
+                    Log.d("TAG", "Sono :")
+                    val database =
+                        FirebaseDatabase.getInstance("https://booktique-87881-default-rtdb.europe-west1.firebasedatabase.app/")
+                    val usersRef = database.reference.child("Utenti")
+                    val childRef = usersRef.child(cUser.uid)
+                    val catalogoRef = childRef.child("Catalogo")
+                    val lettiRef = catalogoRef.child("Letti")
+
+                    if (bookId != null) {
+                            valutazione = 2
+                        graphicLike(valutazione)
+                        lettiRef.child(bookId).child("valutazione").setValue(valutazione)
+                    }
+                }
+            }
+
+        }
+
+        if (binding.textInputLayout.editText != null) {
+            binding.textInputLayout.editText!!.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+
+                }
+
+                override fun onTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    before: Int,
+                    count: Int
+                ) {
+
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    if(s != null && s.isNotEmpty() && s.last() == '\n'){
+                        s.replace(s.length - 1, s.length, "")
+                        if (FirebaseAuth.getInstance().currentUser != null) {
+                            val cUser = FirebaseAuth.getInstance().currentUser!!
+                            Log.d("TAG", "Sono :")
+                            val database =
+                                FirebaseDatabase.getInstance("https://booktique-87881-default-rtdb.europe-west1.firebasedatabase.app/")
+                            val usersRef = database.reference.child("Utenti")
+                            val childRef = usersRef.child(cUser.uid)
+                            val catalogoRef = childRef.child("Catalogo")
+                            val lettiRef = catalogoRef.child("Letti")
+
+                            if (bookId != null) {
+                                    review = s.toString()
+                                lettiRef.child(bookId).child("recensione").setValue(review)
+                            }
+
+                        }
+                    }
+
+                }
+
+            })
+        }
+
+
+    }
+
+    fun graphicLike(valAtt : Int?){
+        if(valAtt == 0){
+            binding.likeL.setImageResource(R.drawable.pollice_icon)
+            binding.dislikeL.setImageResource(R.drawable.pollice_icon)
+        }else if(valAtt == 1){
+            binding.likeL.setImageResource(R.drawable.like_click_icon)
+            binding.dislikeL.setImageResource(R.drawable.pollice_icon)
+        }else{
+            binding.dislikeL.setImageResource(R.drawable.dislike_click)
+            binding.likeL.setImageResource(R.drawable.pollice_icon)
+        }
     }
 }
